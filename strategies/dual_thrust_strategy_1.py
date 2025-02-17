c9c9ee7bebaa125a2bc4c86df8708973b2776479
GuoYongHeng <1547615191@qq.com>
2025-02-17 12:00:10 +0800

    改动dual thrus策略

--- strategies/dual_thrust_strategy_1.py
+++ strategies/dual_thrust_strategy_1.py
@@ -32,12 +32,6 @@ class DualThrustStrategy(CtaTemplate):
     buy_line = 0
     sell_line = 0
 
-    long_entered = False 
-    # 该变量用来保证一个交易日只买一次，
-    # 我也不知道为什么一个交易日只买一次，
-    # 因为vnpy官方实现代码中一个交易日只买一次
-    # 我也就这样做了
-
     parameters = ["n_day", "k1", "k2", "fixed_size"]
     variables = ["range", "buy_line", "sell_line"]
 
@@ -76,22 +70,23 @@ class DualThrustStrategy(CtaTemplate):
         if self.first_bar:
             self.day_open = bar.open_price # 记录当日开盘价
 
-            HH = max(self.am.high_array[-self.n_day : ])  # n日最高价的最高价
-            LC = min(self.am.close_array[-self.n_day : ]) # n日收盘价的最低价
-            HC = max(self.am.close_array[-self.n_day : ]) # n日收盘价的最高价
-            LL = min(self.am.low_array[-self.n_day : ])   # n日最低价的最低价
+            HH = max(self.am.high_array)  # n日最高价的最高价
+            LC = min(self.am.close_array) # n日收盘价的最低价
+            HC = max(self.am.close_array) # n日收盘价的最高价
+            LL = min(self.am.low_array)   # n日最低价的最低价
             self.range = max(HH - LC, HC - LL) # 计算n日的波动幅度
 
             self.buy_line = self.day_open + self.k1 * self.range
             self.sell_line = self.day_open - self.k2 * self.range
 
             self.first_bar = False
-            self.long_entered = False
 
         
         # 不管有没有持仓，只要突破上轨就买入
-        if bar.close_price > self.buy_line and not self.long_entered:
+        if bar.close_price > self.buy_line:
             self.buy(bar.close_price, self.fixed_size)
+            self.buy_line = bar.open_price + self.k1 * self.range
+            self.sell_line = bar.open_price - self.k2 * self.range
 
         # 只要突破下轨，就全部卖出
         if bar.close_price < self.sell_line:
@@ -112,8 +107,7 @@ class DualThrustStrategy(CtaTemplate):
 
 
     def on_trade(self, trade: TradeData):
-        if self.pos > 0:
-            self.long_entered = True
+        pass
 
 
     def on_stop_order(self, stop_order: StopOrder):
